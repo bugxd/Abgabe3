666d062b8cfc78dd1d5c65b4b3c9e580b4b16032
Lukas Feistritzer <luki.feise@gmail.com>
2016-05-08 17:21:53 +0200

    .
    + PBKDF2 Algorythmus laut RFC2898 Seite 8 -10 von
    https://www.ietf.org/rfc/rfc2898.txt

--- src/ab3/impl/Nachnamen/PasswordToolsImpl.java
+++ src/ab3/impl/Nachnamen/PasswordToolsImpl.java
@@ -3,12 +3,15 @@ package ab3.impl.Nachnamen;
 import ab3.PasswordTools;
 
 
+import java.nio.ByteBuffer;
 import java.security.MessageDigest;
 import java.security.SecureRandom;
 import java.util.Random;
 
 public class PasswordToolsImpl implements PasswordTools {
-	
+
+	public static final int hLen = 32;
+
 	@Override
 	public SaltedHash createSaltedHash(String password) {
 		SaltedHash sh = new SaltedHash();
@@ -22,6 +25,10 @@ public class PasswordToolsImpl implements PasswordTools {
 		return sh;
 	}
 
+	/* /
+	* Generate Hash with salt
+	* concatenate password and salt in one array and generate hash with MessageDigest Class
+	* */
 	public static byte[] generateHash(byte[] pw, byte[] salt){
 		MessageDigest md = null;
 		try {
@@ -36,6 +43,9 @@ public class PasswordToolsImpl implements PasswordTools {
 		return md.digest(pw);
 	}
 
+	/* /
+	 * Concatenates two arrays
+	 * */
 	public static byte[] concatArray(byte[] arr1,byte[] arr2){
 		byte[] all = new byte[arr1.length+arr2.length];
 
@@ -50,6 +60,9 @@ public class PasswordToolsImpl implements PasswordTools {
 		return all;
 	}
 
+	/*
+	* Print password hash as hex string for testing
+	* */
 	public static void printHexHash(byte[] byteData){
 		StringBuffer hexString = new StringBuffer();
 		for (int i=0;i<byteData.length;i++) {
@@ -60,6 +73,9 @@ public class PasswordToolsImpl implements PasswordTools {
 		System.out.println(hexString);
 	}
 
+	/*
+	* Generate random salt with SecureRandom Class
+	* */
 	public static byte[] getNextSalt() {
 		Random r = new SecureRandom();
 		byte[] salt = new byte[16];
@@ -74,6 +90,10 @@ public class PasswordToolsImpl implements PasswordTools {
 		return checkArray(hash.getHash(), checkHash);
 	}
 
+	/*
+	* Compares two arrays
+	* return true if equals
+	* */
 	public static boolean checkArray(byte[] arr1,byte[] arr2){
 		if(arr1.length != arr2.length)
 			return false;
@@ -88,8 +108,80 @@ public class PasswordToolsImpl implements PasswordTools {
 
 	@Override
 	public byte[] PBKDF2(byte[] password, byte[] salt, int iterations, int dkLen) {
-		
-		return null;
+		if(dkLen > (Math.pow(2,32) - 1) * hLen){
+            System.out.println("derived key too long");
+			return null;
+		}
+
+        //the number of hLen-byte blocks in the derived key
+        //round up to the next greater or equal integer
+		int l =  (dkLen - 1) / hLen + 1;
+        //the number of byte in the last block
+		int r = dkLen - (l - 1) * hLen;//l minus eins
+
+        byte[] dk = getT(password,salt,iterations,1);
+        for(int i = 2; i < dkLen; i++){
+            byte[] T_I =  getT(password,salt,iterations,i);
+            byte[] dk_help = dk;
+            dk = concatArray(dk_help,T_I);
+
+        }
+
+        byte[] DK = getArrayValues(dk, dkLen);
+
+        if(DK != null)
+		    return DK;
+        else
+            return null;
 	}
+
+    public byte[] getArrayValues(byte[] arr, int x){
+        if(arr.length < x)
+            return null;
+
+        byte[] ret = new byte[x];
+        for(int i = 0; i < x; i++){
+            ret[i] = arr[i];
+        }
+
+        return ret;
+    }
+
+    /*
+    * recursive implementation to get block t_i from DK
+    * */
+    public byte[] getT(byte[] P, byte[] S, int c, int i){
+
+        if(c == 1){
+            return  calculateU_1(P,S,c,getByteOfInt(i));
+        }
+        else{
+            return generateHash(P,getT(P,S,--c,i));
+        }
+    }
+
+    public byte[] calculateU_1(byte[] P, byte[] S, int c, byte[] i){
+        return generateHash(P, concatArray(S,i));
+    }
+
+    /*
+    * return first xor second for each byte
+    * */
+    public byte[] xorArray(byte[] first, byte[] second){
+        byte[] result = new byte[Math.min(first.length, second.length)];
+
+        for (int i = 0; i < result.length; i++) {
+            result[i] = (byte) (((int) first[i]) ^ ((int) second[i]));
+        }
+
+        return result;
+    }
+
+    /*
+    * returns int value in 4 byte
+    * */
+    public byte[] getByteOfInt(int i){
+        return ByteBuffer.allocate(4).putInt(i).array();
+    }
 
 }
--- src/ab3/main.java
+++ src/ab3/main.java
@@ -12,15 +12,24 @@ public class main {
     {
         PasswordToolsImpl pti = new PasswordToolsImpl();
 
-        PasswordTools.SaltedHash sh = pti.createSaltedHash("luki");
+        PasswordToolsImpl.printHexHash(pti.PBKDF2("password".getBytes(), "salt".getBytes(), 4096, 256));
 
-        if(pti.checkSaltedHash("luki",sh))
+
+        String s = "e2c690b0d6ff6086ad37af7e773a22d33c269e481eb0820b648408765c6c92a09865378e7b7f750cbe963d744f4d9ad5dc33d37344b68ddc7523329ac4624aa3c0c0661cfc18dc397a48c5ac67f9992e38cdb3dc7aa92634c0fd93c3b529539c2444508cda1d23ae09c433c733b04e4c64eb8f74b35aefe76746795a8cf052ce8b3a8dc6ff19264264b7e2867948a16424f7a973d618d38e6075b569e053c5bcdf5a01df8ff0111a7dbc9a12b41172d91a28234e4b63d6a60ea49225deb9a252e5c321abfc312704c617bb47ff4d9e54e4b438aaa612625b6143546bb64c3b70d4124edb8e534e264990743f622867d401a5fe1049c95269405fc0293eca415c";
+        System.out.println(s.length());
+
+
+
+        /*
+        PasswordTools.SaltedHash sh = pti.createSaltedHash("password");
+
+        if(pti.checkSaltedHash("password",sh))
             System.out.print("correct");
         else
             System.out.println("false");
 
 
-
+        */
 
        /* String password = "123456";
 
